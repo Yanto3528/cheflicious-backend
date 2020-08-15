49aa8a9e11b7c04a926e843422fd07ae09ff76f2
Yanto Lee <yanto3528@gmail.com>
2020-08-15 14:31:11 +0700

    configure cors

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
